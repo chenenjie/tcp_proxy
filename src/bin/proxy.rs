78ea9f59be7e5bbcc4512a2e8888644402800837
chenenjie <chenenjiejiang@gmail.com>
2017-09-16 18:31:56 +0800

    update

---
+++ src/bin/proxy.rs
@@ -0,0 +1,9 @@
+extern crate tcp_proxy; 
+
+use tcp_proxy::serve;
+
+
+fn main(){
+    serve();
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,13 @@ extern crate tokio_io;
 extern crate error_chain;
 
 use tokio_core::reactor::{Core, Handle};
-use tokio_core::net::{Incoming, TcpListener };
-use tokio_io::AsyncRead;
-use futures::{Stream,Sink};
+use tokio_core::net::{TcpListener };
+use tokio_core::net::TcpStream;
+use tokio_io::{ AsyncRead};
+use tokio_io::io;
+use futures::{Stream};
+use futures::future::Future;
+use std::io::{ErrorKind};
 
 
 mod proxy {
@@ -26,8 +30,8 @@ mod proxy {
 }
 
 
-fn serve() -> proxy::Result<()>{
-    let core = Core::new().unwrap();
+pub fn serve() -> proxy::Result<()>{
+    let mut core = Core::new().unwrap();
     let handle = core.handle();
 
     let addr = "0.0.0.0:12345".parse().unwrap();
@@ -38,10 +42,29 @@ fn serve() -> proxy::Result<()>{
     let server = connections.for_each(move |(socket, _peer_addr)| {
         let (writer, reader) = socket.split();
 
+        let target_addr = "127.0.0.1:10086".parse().unwrap();
+        let peer_stream = TcpStream::connect(&target_addr, &handle);
+
+        let deal = peer_stream.and_then(|stream| {
+            let (peer_w, peer_r) = stream.split();
+        
+            let push = io::copy(writer, peer_r);
+            let pull = io::copy(peer_w, reader);
+
+            push.select(pull).then(|result|{
+                match result {
+                    Err((err, _)) => Err(ErrorKind::Other.into()),
+                    Ok((item, _)) => Ok(item),
+                }
+            })
+        }).then(|_| Ok(()));
+
+        
+        handle.spawn(deal);
         
         Ok(())
     });
 
-    core.run(server)
+    core.run(server).map_err(From::from)
 
 }
